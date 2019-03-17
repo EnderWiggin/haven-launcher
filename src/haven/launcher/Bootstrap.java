3ac74b053b5647abb4dc9729538f98623d13efd1
Fredrik Tolf <fredrik@dolda2000.com>
2019-03-17 02:30:09 +0100

    Added bootstrapper.

---
+++ src/haven/launcher/Bootstrap.java
@@ -0,0 +1,88 @@
+/*
+ *  This file is part of the Haven Java Launcher.
+ *  Copyright (C) 2019 Fredrik Tolf <fredrik@dolda2000.com>, and
+ *                     Björn Johannessen <johannessen.bjorn@gmail.com>
+ *
+ *  Redistribution and/or modification of this file is subject to the
+ *  terms of the GNU Lesser General Public License, version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Other parts of this source tree adhere to other copying
+ *  rights. Please see the file `COPYING' in the root directory of the
+ *  source tree for details.
+ *
+ *  A copy the GNU Lesser General Public License is distributed along
+ *  with the source tree of which this file is a part in the file
+ *  `doc/LPGL-3'. If it is missing for any reason, please see the Free
+ *  Software Foundation's website at <http://www.fsf.org/>, or write
+ *  to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+package haven.launcher;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.zip.*;
+
+public class Bootstrap {
+    public static void bootstrap(OutputStream out, InputStream cfg) throws IOException {
+	File jarpath = null;
+	if(!(Bootstrap.class.getClassLoader() instanceof URLClassLoader))
+	    throw(new RuntimeException("Can only bootstrap via URL classloaders"));
+	for(URL url : ((URLClassLoader)Bootstrap.class.getClassLoader()).getURLs()) {
+	    if(url.getProtocol().equals("file")) {
+		File cpath = new File(url.getFile());
+		if(cpath.isFile()) {
+		    try(ZipFile jar = new ZipFile(cpath)) {
+			if(jar.getEntry("haven/launcher/Bootstrap.class") != null) {
+			    jarpath = cpath;
+			    break;
+			}
+		    }
+		}
+	    }
+	}
+	if(jarpath == null)
+	    throw(new RuntimeException("Could not find launcher Jar on classpath"));
+	ZipOutputStream outjar = new ZipOutputStream(out);
+	byte[] buf = new byte[65536];
+	try(ZipFile injar = new ZipFile(jarpath)) {
+	    for(Enumeration<? extends ZipEntry> entries = injar.entries(); entries.hasMoreElements();) {
+		ZipEntry ent = entries.nextElement();
+		if(ent.getName().equals("haven/launcher/bootstrap.hl")) {
+		    System.err.println("launcher: warning: removing current bootstrap");
+		} else {
+		    outjar.putNextEntry(ent);
+		    try(InputStream cont = injar.getInputStream(ent)) {
+			for(int rv = cont.read(buf); rv >= 0; rv = cont.read(buf))
+			    outjar.write(buf, 0, rv);
+		    }
+		}
+	    }
+	}
+	outjar.putNextEntry(new ZipEntry("haven/launcher/bootstrap.hl"));
+	for(int rv = cfg.read(buf); rv >= 0; rv = cfg.read(buf))
+	    outjar.write(buf, 0, rv);
+	outjar.finish();
+    }
+
+    public static void main(String[] args) {
+	try {
+	    try(InputStream cfg = new BufferedInputStream(new FileInputStream(args[0]))) {
+		try(OutputStream out = new BufferedOutputStream(new FileOutputStream(args[1]));) {
+		    bootstrap(out, cfg);
+		}
+	    }
+	} catch(Exception e) {
+	    e.printStackTrace();
+	    System.exit(1);
+	}
+    }
+}
